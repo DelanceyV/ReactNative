8ff4ff02c11428c64d41278dee9ee358670f3fd7
Delancey Vela <delancey.sheldon@gmail.com>
2021-04-28 12:26:27 -0400

    share

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { View, Text, ScrollView, FlatList } from 'react-native';
-import { Card, ListItem } from 'react-native-elements';
+import { Card, Button, Icon } from 'react-native-elements';
 import { CAMPSITES } from '../shared/campsites';
 import * as Animatable from 'react-native-animatable';
+import * as MailComposer from 'expo-mail-composer';
 
 class Contact extends Component {
 
@@ -15,6 +16,14 @@ class Contact extends Component {
 
     static navigationOptions = {
         title: 'Contact Us'
+    }
+
+    sendMail() {
+      MailComposer.composeAsync({
+        recipients: ['campsites@nucamp.co'],
+        subject: 'Inquiry',
+        body: 'To whom it may concern:'
+      });
     }
 
     render() {
@@ -30,6 +39,17 @@ class Contact extends Component {
                         <Text style={{marginBottom: 10}}>U.S.A.</Text>
                         <Text>Phone: 1-206-555-1234</Text>
                         <Text>Email: campsites@nucamp.co</Text>
+                        <Button 
+                          title='Send Email'
+                          butttonStyle={{backgroundColor: '#5637DD', margin: 40}}
+                          icon={<Icon
+                              name='envelope-o'
+                              type='font-awesome'
+                              color='#fff'
+                              iconStyle={{marginRight: 10}}
+                          />}
+                          onPress={() => this.sendMail()}
+                        />
                     </Card>
                 </Animatable.View>
             </ScrollView>
